f2a81d64c931f66432933dc952cf333dc530ef26
Shortcake Sweets (정동원) <olleh2020@kaist.ac.kr>
2025-11-03 10:58:15 +0900

    Fix list generator

--- src/node_js/list_gen.mjs
+++ src/node_js/list_gen.mjs
@@ -1,7 +1,7 @@
 import fs from "node:fs/promises";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-import { createChartFromBin } from "rift_essentials.mjs";
+import { createChartFromBin } from "./rift_essentials.mjs";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
